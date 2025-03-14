a9310c448b095742dc1c908a08f5644b266affce
Yanarkaev <hasssambek@gmail.com>
2025-03-14 18:06:19 +0300

    почти почти все

--- src/entities/Note/Note.tsx
+++ src/entities/Note/Note.tsx
@@ -6,37 +6,14 @@ import { INote } from "./model/types/noteSchema";
 interface IProps extends React.HTMLAttributes<HTMLDivElement>, INote {}
 
 export const Note = ({ text, _id, textColor, backgroundColor }: IProps) => {
-  //   const [isEditable, setIsEditable] = useState(false);
-//   const [value, setValue] = useState(text);
-
-//   const handleInput = (e: React.FormEvent<HTMLDivElement>) => {
-//     setValue(e.currentTarget.textContent || "");
-//   };
-
-//   console.log(value);
-
   return (
     <div
       className={s.Note}
-      //   className={`${s.Note} ${isEditable ? s.editable : ""}`}
-      //   onClick={() => setIsEditable(true)}
       style={{ color: textColor, backgroundColor: backgroundColor }}
     >
       <DeleteNote className={s.delete} _id={_id} />
       <ChangeColors _id={_id} className={s.changeColors} />
-      {/* <div
-        className={s.inner}
-    
-
-      > */}
-        {/* {isEditable ? (
-          <EditNote _id={_id} text={text} setIsEditable={setIsEditable} />
-        ) : (
-          text
-        )} */}
-        {/* {text}
-      </div> */}
-      <EditNote text={text} _id={_id}/>
+      <EditNote text={text} _id={_id} />
     </div>
   );
 };
--- src/features/editNote/EditNote.tsx
+++ src/features/editNote/EditNote.tsx
@@ -1,67 +1,50 @@
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "../../app/store/hooks";
 import { noteActions } from "../../entities/Note/model/slice/noteSlice";
 import s from "./EditNote.module.scss";
 
 interface IProps extends React.HTMLAttributes<HTMLTextAreaElement> {
-  setIsEditable?: (arg: any) => void;
   _id: number;
   text: string;
 }
 
-export const EditNote = ({ setIsEditable, _id, text }: IProps) => {
+export const EditNote = ({ _id, text }: IProps) => {
   const dispatch = useAppDispatch();
-  //   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  //   const [inputValue, setInputValue] = useState(text);
-
-  //   const maxLength = 100;
-
-  //   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //     const currentText = e.target.value;
-  //     const lineBreaksCount = (currentText.match(/\n/g) || []).length * 10;
-  //     const totalTextLength = currentText.length + lineBreaksCount;
-  //     console.log(totalTextLength);
-
-  //     // if (totalTextLength < maxLength) {
-  //     setInputValue(currentText);
-  //     // }
-  //   };
 
   const [value, setValue] = useState(text);
 
   const handleInput = (e: React.FormEvent<HTMLDivElement>) => {
-    setValue(e.currentTarget.innerText || "");
+    setValue(
+      e.currentTarget.innerHTML
+        .replace(/<div>/g, "\n")
+        .replace(/<\/div>/g, "")
+        .replace(/<br>/g, "")
+        .replace(/<p>/g, "")
+        .replace(/<\/p>/g, "") || ""
+    );
   };
 
-  console.log(value);
+  console.log(`${value}`);
 
   const handleEdit = () => {
     dispatch(noteActions.editNote({ _id, text: value }));
-    // setIsEditable(false);
   };
 
+  useEffect(() => {
+    window.addEventListener("beforeunload", handleEdit);
+    
+    return () => {
+      window.removeEventListener("beforeunload", handleEdit);
+    };
+  });
+
   return (
-    // <textarea
-    //   name=""
-    //   className={s.EditNote}
-    //   onBlur={handleEdit}
-    //   ref={textareaRef}
-    //   defaultValue={text}
-    //   onChange={handleChange}
-    //   value={inputValue}
-    //   autoFocus
-    // ></textarea>
     <div
       className={s.EditNote}
       contentEditable={true}
       onInput={handleInput}
       onBlur={handleEdit}
       suppressContentEditableWarning={true}
-      style={{
-        whiteSpace: "pre-wrap",
-        border: "1px solid #ccc",
-        padding: "10px",
-      }}
     >
       {text}
     </div>
